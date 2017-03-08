cbe93d3fa63c63d6b2e3ac4eddf1800de37bae4a
mirteu <mirteu@gmail.com>
2017-03-08 09:26:14 +0000

    Initial project creation - blank c++

---
+++ SpaceInvaders/Source/SpaceInvaders/SpaceInvadersGameModeBase.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameModeBase.h"
+#include "SpaceInvadersGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SPACEINVADERS_API ASpaceInvadersGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
